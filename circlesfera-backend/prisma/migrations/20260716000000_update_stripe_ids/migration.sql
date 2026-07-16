ee74fd26752c2cd8f4bf773810b1b936bf5a3312
EasyFeliu <easyfeliu@gmail.com>
2026-07-16 03:21:17 +0200

    chore(database): add migration to update Stripe IDs in production DB

---
+++ circlesfera-backend/prisma/migrations/20260716000000_update_stripe_ids/migration.sql
@@ -0,0 +1,3 @@
+UPDATE "PlatformPlan" SET "stripeProductId" = 'prod_UtQGHGBnYo5yGX', "stripePriceId" = 'price_1TtdPZIEniBX3suALJ68LF3d' WHERE name = 'Premium';
+UPDATE "PlatformPlan" SET "stripeProductId" = 'prod_UtQG21Jd98Vidi', "stripePriceId" = 'price_1TtdPZIEniBX3suAnR6uNNsN' WHERE name = 'Elite Creator';
+UPDATE "PlatformPlan" SET "stripeProductId" = 'prod_UtQGy36G3SscjF', "stripePriceId" = 'price_1TtdPaIEniBX3suAm9IfVW1o' WHERE name = 'Business';
